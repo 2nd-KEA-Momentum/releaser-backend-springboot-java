9659a4372747e2309c3e2985cff902da63689798
seonwoo <ska2870ghk@naver.com>
2023-07-04 13:37:48 +0900

    fix: Remove unnecessary response code

--- src/main/java/com/momentum/releaser/global/config/BaseResponseStatus.java
+++ src/main/java/com/momentum/releaser/global/config/BaseResponseStatus.java
@@ -16,43 +16,15 @@ public enum BaseResponseStatus {
      * 2000 : Request 오류
      */
 
-    // Common
-    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
-    EMPTY_JWT(false, 2001, "JWT를 입력해 주세요."),
-    INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
-    INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
-    INVALID_REQUEST_BODY(false, 2005, "요청 데이터가 잘못되었습니다."),
-
-
-    // users
-    USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해 주세요."),
-
-    // [POST] /users
-    EMPTY_EMAIL(false, 2015, "이메일을 입력해 주세요."),
-    EMPTY_PASSWORD(false, 2016, "비밀번호를 입력해 주세요."),
-    INVALID_EMAIL(false, 2017, "이메일 형식을 확인해 주세요."),
-    INVALID_PASSWORD(false, 2018, "비밀번호는 영어와 숫자만 사용하세요."),
-    EXISTS_EMAIL(false,2019,"중복된 이메일입니다."),
-    NOT_EXISTS_USER(false, 2020, "존재하지 않는 회원입니다."),
-    EXISTS_USER(false, 2021, "이미 존재하는 회원입니다."),
-    FAILED_TO_UPDATE_USER(false,2022,"사용자 정보 수정에 실패하였습니다"),
     /**
      * 3000 : Response 오류
      */
-    // Common
-    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
-
-    // [POST] /users
-    DUPLICATED_EMAIL(false, 3013, "중복된 이메일입니다."),
-    FAILED_TO_LOGIN(false,3014,"비밀번호가 틀렸습니다."),
 
     /**
      * 4000 : Database, Server 오류
      */
     DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
-    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
-    PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
-    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
+    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다.");
 
     private final boolean isSuccess;
     private final int code;
