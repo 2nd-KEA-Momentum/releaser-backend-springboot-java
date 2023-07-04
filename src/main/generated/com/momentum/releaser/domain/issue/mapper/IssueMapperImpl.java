a1f67c5ebf8e556ebcd00b0c6699c9309db5fe02
seonwoo <ska2870ghk@naver.com>
2023-07-04 09:33:43 +0900

    comment: @RequiredArgsConstructor 주석 변경

--- src/main/generated/com/momentum/releaser/domain/issue/mapper/IssueMapperImpl.java
+++ src/main/generated/com/momentum/releaser/domain/issue/mapper/IssueMapperImpl.java
@@ -4,8 +4,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-03T15:59:03+0900",
-    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 11.0.16 (Azul Systems, Inc.)"
+    date = "2023-07-04T09:30:54+0900",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 19.0.2 (Oracle Corporation)"
 )
 public class IssueMapperImpl implements IssueMapper {
 }
