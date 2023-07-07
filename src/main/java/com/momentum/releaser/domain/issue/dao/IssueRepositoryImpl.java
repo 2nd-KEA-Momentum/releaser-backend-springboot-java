46e314690ba9f1d6dc75f5e4216bdea5ea08bc74
chaeanna <annabcd99@naver.com>
2023-07-07 10:02:26 +0900

    feat : Create IssueNum Entity
    - 이슈 번호 테이블 생성

--- src/main/java/com/momentum/releaser/domain/issue/dao/IssueRepositoryImpl.java
+++ src/main/java/com/momentum/releaser/domain/issue/dao/IssueRepositoryImpl.java
@@ -1,9 +1,17 @@
 package com.momentum.releaser.domain.issue.dao;
 
+import com.momentum.releaser.domain.issue.domain.QIssue;
+import com.momentum.releaser.domain.issue.dto.IssueResDto.IssueInfoRes;
+import com.momentum.releaser.domain.issue.dto.QIssueResDto_IssueInfoRes;
+import com.momentum.releaser.domain.project.domain.QProjectMember;
+import com.momentum.releaser.domain.release.domain.QReleaseNote;
+import com.momentum.releaser.domain.user.domain.QUser;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Slf4j
@@ -12,30 +20,31 @@ import org.springframework.stereotype.Repository;
 public class IssueRepositoryImpl implements IssueRepositoryCustom{
     private final JPAQueryFactory queryFactory;
 
-//    @Override
-//    public List<IssueInfoRes> getIssues() {
-//        QIssue issue = QIssue.issue;
-//        QProjectMember member = QProjectMember.projectMember;
-//        QUser user = QUser.user;
-//        QReleaseNote releaseNote = QReleaseNote.releaseNote;
-//
-//        List<IssueInfoRes> fetch = queryFactory
-//                .select(
-//                        new QIssueResDto_IssueInfoRes(
-//                                issue.issueId,
-//                                issue.title,
-//                                issue.content,
-//                                member.memberId,
-//                                user.name,
-//                                user.img,
-//                                issue.tag,
-//                                releaseNote.version,
-//                                issue.edit,
-//                                issue.lifeCycle)
-//                )
-//                .from()
-//                .where()
-//                .fetch();
-//        return null;
-//    }
+    @Override
+    public List<IssueInfoRes> getIssues() {
+        QIssue issue = QIssue.issue;
+        QProjectMember member = QProjectMember.projectMember;
+        QUser user = QUser.user;
+        QReleaseNote releaseNote = QReleaseNote.releaseNote;
+
+
+        List<IssueInfoRes> fetch = queryFactory
+                .select(
+                        new QIssueResDto_IssueInfoRes(
+                                issue.issueId,
+                                issue.title,
+                                issue.content,
+                                member.memberId,
+                                user.name,
+                                user.img,
+                                issue.tag.stringValue(),
+                                releaseNote.version,
+                                issue.edit,
+                                issue.lifeCycle.stringValue())
+                )
+                .from()
+                .where()
+                .fetch();
+        return null;
+    }
 }
