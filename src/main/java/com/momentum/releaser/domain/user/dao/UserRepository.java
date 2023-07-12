fe890ce7bbba3a492fc60e827765291eb1e5cd99
chaeanna <annabcd99@naver.com>
2023-07-12 11:06:15 +0900

    refactor : application.yml

--- src/main/java/com/momentum/releaser/domain/user/dao/UserRepository.java
+++ src/main/java/com/momentum/releaser/domain/user/dao/UserRepository.java
@@ -4,6 +4,10 @@ import com.momentum.releaser.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(collectionResourceRel="user", path="user")
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findOneByEmail(String email);
 }
