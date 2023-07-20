acbffdb157a099a04dbc49c85c748338bd00e78f
chaeanna <annabcd99@naver.com>
2023-07-20 09:46:36 +0900

    feat : API 4.2 프로젝트 멤버 추가 구현 중

--- src/main/java/com/momentum/releaser/domain/project/dao/ProjectRepository.java
+++ src/main/java/com/momentum/releaser/domain/project/dao/ProjectRepository.java
@@ -4,6 +4,9 @@ import com.momentum.releaser.domain.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(collectionResourceRel="project", path="project")
 public interface ProjectRepository extends JpaRepository<Project, Long> , ProjectRepositoryCustom{
+    Optional<Project> findByLink(String link);
 }
