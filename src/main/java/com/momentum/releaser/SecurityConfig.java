0001cbc807d681e7c4da5e2fb7eaedbc89603c78
chaeanna <annabcd99@naver.com>
2023-07-24 10:43:24 +0900

    comment : cors 주석 처리

--- src/main/java/com/momentum/releaser/SecurityConfig.java
+++ src/main/java/com/momentum/releaser/SecurityConfig.java
@@ -9,7 +9,7 @@
 //import org.springframework.web.cors.CorsConfigurationSource;
 //import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 //
-////@EnableWebSecurity
+//@EnableWebSecurity
 //public class SecurityConfig {
 //
 ////    @Bean
