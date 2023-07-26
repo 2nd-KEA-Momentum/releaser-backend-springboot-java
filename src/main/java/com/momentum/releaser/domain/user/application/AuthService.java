a2682b8a49a164c34e5681afea605cb44fbbd87c
chaeanna <annabcd99@naver.com>
2023-07-26 20:09:24 +0900

    REL-203 refactor : auth refactoring
    - 주석 처리

--- src/main/java/com/momentum/releaser/domain/user/application/AuthService.java
+++ src/main/java/com/momentum/releaser/domain/user/application/AuthService.java
@@ -13,33 +13,17 @@ public interface AuthService {
 
     /**
      * 2.1 회원가입
-     *
-     * 사용자 정보를 받아 회원가입을 처리합니다.
-     *
-     * @param userInfoReq 회원가입 요청 객체
-     * @return UserInfoResponseDTO 회원가입 성공 시 사용자 정보를 담은 DTO
      */
     UserInfoResponseDTO addSignUpUser(UserInfoReqestDTO userInfoReq);
 
 
     /**
      * 2.2 이메일 로그인
-     *
-     * 이메일과 비밀번호로 사용자를 로그인합니다.
-     *
-     * @param userLoginReq 로그인 요청 객체
-     * @return TokenDto 로그인 성공 시 토큰 정보를 담은 DTO
      */
     TokenDto saveLoginUser(UserLoginReqestDTO userLoginReq);
 
     /**
      * 2.3 Token 재발급
-     *
-     * 기존의 Access Token과 Refresh Token을 사용하여 새로운 Access Token을 발급받습니다.
-     *
-     * @param accessToken  기존의 Access Token
-     * @param refreshToken 기존의 Refresh Token
-     * @return TokenDto Token 재발급 성공 시 새로운 Access Token 정보를 담은 DTO
      */
     TokenDto saveRefreshUser(String accessToken, String refreshToken);
 
