b9dcaa05164705ddf482425442fdadcc398294ec
chaeanna <annabcd99@naver.com>
2023-07-31 22:32:57 +0900

    REL-207 refactor : releaseOpinion refactoring
    - 주석 처리

--- src/main/java/com/momentum/releaser/domain/release/api/ReleaseController.java
+++ src/main/java/com/momentum/releaser/domain/release/api/ReleaseController.java
@@ -136,6 +136,9 @@ public class ReleaseController {
 
     /**
      * 6.1 릴리즈 노트 의견 추가
+     *
+     * @param userPrincipal 인증된 사용자 정보를 담고 있는 객체
+     * @return ReleaseOpinionsResponseDTO 릴리즈 의견 정보를 담은 응답 DTO
      */
     @PostMapping(value = "/{releaseId}/opinions")
     public BaseResponse<List<ReleaseOpinionsResponseDTO>> releaseOpinionAdd(@AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -147,6 +150,10 @@ public class ReleaseController {
 
     /**
      * 6.2 릴리즈 노트 의견 삭제
+     *
+     * @param userPrincipal 인증된 사용자 정보를 담고 있는 객체
+     * @param opinionId 릴리즈 의견 식별 번호
+     * @return ReleaseOpinionsResponseDTO 릴리즈 의견 정보를 담은 응답 DTO
      */
     @PostMapping("/opinions/{opinionId}")
     public BaseResponse<List<ReleaseOpinionsResponseDTO>> releaseOpinionRemove(@AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -157,6 +164,9 @@ public class ReleaseController {
 
     /**
      * 6.3 릴리즈 노트 의견 목록 조회
+     *
+     * @param releaseId 릴리즈 식별 번호
+     * @return ReleaseOpinionsResponseDTO 릴리즈 의견 정보를 담은 응답 DTO
      */
     @GetMapping("/{releaseId}/opinions")
     public BaseResponse<List<ReleaseOpinionsResponseDTO>> releaseOpinionList(@PathVariable @Min(value = 1, message = "릴리즈 식별 번호는 1 이상의 숫자여야 합니다.") Long releaseId) {
