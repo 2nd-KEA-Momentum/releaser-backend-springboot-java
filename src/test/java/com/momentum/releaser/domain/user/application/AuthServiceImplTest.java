5be158873a29e2fe305109001c2a1c55e40002b5
chaeanna <annabcd99@naver.com>
2023-08-11 20:01:21 +0900

    REL-317 comment : Auth test

--- src/test/java/com/momentum/releaser/domain/user/application/AuthServiceImplTest.java
+++ src/test/java/com/momentum/releaser/domain/user/application/AuthServiceImplTest.java
@@ -1,43 +1,29 @@
 package com.momentum.releaser.domain.user.application;
 
-import com.momentum.releaser.domain.issue.application.IssueServiceImpl;
-import com.momentum.releaser.domain.issue.dao.IssueNumRepository;
-import com.momentum.releaser.domain.issue.dao.IssueOpinionRepository;
-import com.momentum.releaser.domain.issue.dao.IssueRepository;
-import com.momentum.releaser.domain.project.dao.ProjectMemberRepository;
-import com.momentum.releaser.domain.project.dao.ProjectRepository;
-import com.momentum.releaser.domain.release.dao.release.ReleaseRepository;
-import com.momentum.releaser.domain.user.dao.AuthPasswordRepository;
-import com.momentum.releaser.domain.user.dao.RefreshTokenRepository;
-import com.momentum.releaser.domain.user.dao.UserRepository;
-import com.momentum.releaser.domain.user.domain.AuthPassword;
-import com.momentum.releaser.domain.user.domain.RefreshToken;
-import com.momentum.releaser.domain.user.domain.User;
-import com.momentum.releaser.domain.user.dto.AuthRequestDto;
-import com.momentum.releaser.domain.user.dto.AuthRequestDto.UserInfoReqestDTO;
-import com.momentum.releaser.domain.user.dto.AuthRequestDto.UserLoginReqestDTO;
-import com.momentum.releaser.domain.user.dto.AuthResponseDto;
-import com.momentum.releaser.domain.user.dto.AuthResponseDto.UserInfoResponseDTO;
-import com.momentum.releaser.domain.user.dto.TokenDto;
-import com.momentum.releaser.global.jwt.JwtTokenProvider;
-import com.momentum.releaser.global.security.CustomUserDetailsService;
-import com.momentum.releaser.redis.RedisUtil;
-import com.momentum.releaser.redis.password.PasswordRedisRepository;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import com.momentum.releaser.domain.user.dao.AuthPasswordRepository;
+import com.momentum.releaser.domain.user.dao.RefreshTokenRepository;
+import com.momentum.releaser.domain.user.dao.UserRepository;
+import com.momentum.releaser.domain.user.domain.AuthPassword;
+import com.momentum.releaser.domain.user.domain.User;
+import com.momentum.releaser.domain.user.dto.AuthRequestDto.UserInfoReqestDTO;
+import com.momentum.releaser.domain.user.dto.AuthResponseDto.UserInfoResponseDTO;
+import com.momentum.releaser.global.jwt.JwtTokenProvider;
+import com.momentum.releaser.redis.RedisUtil;
+import com.momentum.releaser.redis.password.PasswordRedisRepository;
 
 class AuthServiceImplTest {
     
@@ -50,7 +36,6 @@ class AuthServiceImplTest {
     private AuthenticationManagerBuilder authenticationManagerBuilder;
     private JwtTokenProvider jwtTokenProvider;
     private ModelMapper modelMapper;
-
     private RedisUtil redisUtil;
     private PasswordRedisRepository passwordRedisRepository;
 
@@ -74,6 +59,7 @@ class AuthServiceImplTest {
     @Test
     @DisplayName("2.1 회원가입")
     void testAddSignUpUser() {
+        // 테스트를 위한 mock 회원가입 정보 생성
         UserInfoReqestDTO mockReqDTO = new UserInfoReqestDTO(
                 "testUser", "testUser@releaser.com", "password"
         );
@@ -87,24 +73,34 @@ class AuthServiceImplTest {
                 1L, "testUser", "testUser@releaser.com"
         );
 
+        // userRepository.findByEmail() 메서드가 빈 결과를 반환하도록 설정 -> 회원이 아닌 경우
         when(userRepository.findByEmail(mockReqDTO.getEmail())).thenReturn(Optional.empty());
+
+        // userRepository.save() 메서드가 mock 유저를 반환하도록 설정
         when(userRepository.save(modelMapper.map(mockReqDTO, User.class))).thenReturn(mockUser);
+
+        // passwordEncoder.encode() 메서드가 mock 비밀번호를 반환하도록 설정
         when(passwordEncoder.encode(mockReqDTO.getPassword())).thenReturn("encryptedPassword");
+
+        // authPasswordRepository.save() 메서드가 mock 비밀번호 정보를 반환하도록 설정
         when(authPasswordRepository.save(any())).thenReturn(mockPassword);
+
+        // modelMapper.map() 메서드가 mock 회원 정보를 반환하도록 설정
         when(modelMapper.map(mockUser, UserInfoResponseDTO.class)).thenReturn(userResDTO);
 
+        // 회원가입 서비스 호출
         UserInfoResponseDTO result = authService.addSignUpUser(mockReqDTO);
 
+        // 예상된 결과와 실제 결과 비교
         assertEquals(userResDTO.getName(), result.getName());
         assertEquals(userResDTO.getEmail(), result.getEmail());
 
+        // 각 메서드가 호출됐는지 확인
         verify(userRepository, times(1)).findByEmail(mockReqDTO.getEmail());
         verify(userRepository, times(1)).save(modelMapper.map(mockReqDTO, User.class));
         verify(passwordEncoder, times(1)).encode(mockReqDTO.getPassword());
         verify(authPasswordRepository, times(1)).save(any(AuthPassword.class));
         verify(modelMapper, times(1)).map(mockUser, UserInfoResponseDTO.class);
     }
-
-
 
 }
