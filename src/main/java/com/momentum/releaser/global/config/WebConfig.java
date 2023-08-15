a34ccf208d0877ad708527fe35d243692dab6463
seonwoo <ska2870ghk@naver.com>
2023-08-15 22:40:31 +0900

    fix: CORS에 /api 추가

--- src/main/java/com/momentum/releaser/global/config/WebConfig.java
+++ src/main/java/com/momentum/releaser/global/config/WebConfig.java
@@ -12,11 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-//                .allowCredentials(true)
+                .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
     }
 
