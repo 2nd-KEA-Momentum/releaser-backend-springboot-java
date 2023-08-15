7836e131d93b54ac35c04b27d419bdbd30891619
seonwoo <ska2870ghk@naver.com>
2023-08-15 22:58:54 +0900

    fix: CORS 테스트

--- src/main/java/com/momentum/releaser/global/config/WebConfig.java
+++ src/main/java/com/momentum/releaser/global/config/WebConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    private final long MAX_AGE_SECS = 3600;
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/oauth2/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true)
-//                .maxAge(MAX_AGE_SECS);
-//    }
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/oauth2/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
