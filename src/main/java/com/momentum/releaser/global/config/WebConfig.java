c4daba27ef9b953948a63b08b66d3c1f998d35d4
seonwoo <ska2870ghk@naver.com>
2023-08-15 22:16:59 +0900

    fix: CORS 부분 제거 테스트

--- src/main/java/com/momentum/releaser/global/config/WebConfig.java
+++ src/main/java/com/momentum/releaser/global/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.momentum.releaser.global.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,15 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(MAX_AGE_SECS);
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
