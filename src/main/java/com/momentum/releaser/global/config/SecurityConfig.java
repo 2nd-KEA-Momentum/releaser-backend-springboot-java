f97777e39bc97bc4e1c713da9af58473364159a8
chaeanna <annabcd99@naver.com>
2023-08-15 23:25:44 +0900

    REL-68 chore : active dev

--- src/main/java/com/momentum/releaser/global/config/SecurityConfig.java
+++ src/main/java/com/momentum/releaser/global/config/SecurityConfig.java
@@ -115,18 +115,8 @@ public class SecurityConfig {
         return new JwtAuthenticationFilter(jwtTokenProvider);
     }
 
-    /**
-     * httpBasic().disable().csrf().disable(): rest api이므로 basic auth 및 csrf 보안을 사용하지 않는다는 설정
-     * sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS): JWT를 사용하기 때문에 세션을 사용하지 않는다는 설정
-     * antMatchers().permitAll(): 해당 API에 대해서는 모든 요청을 허가한다는 설정
-     * antMatchers().hasRole("USER"): USER 권한이 있어야 요청할 수 있다는 설정
-     * anyRequest().authenticated(): 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
-     * addFilterBefore(new JwtAUthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class): JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행하겠다는 설정
-     * passwordEncoder: JWT를 사용하기 위해서는 기본적으로 password encoder가 필요한데, 여기서는 Bycrypt encoder를 사용
-     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
 
         http
                 .cors()
